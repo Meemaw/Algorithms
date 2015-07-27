eaaa363530fc70a5c5bba2f5f1c946237b21d5bd
Meemaw <totalfr33k@gmail.com>
2015-07-27 21:27:24 +0200

    Median of three implementation

--- Sorts/Quicksort.py
+++ Sorts/Quicksort.py
@@ -1,12 +1,23 @@
 __author__ = 'Meemaw'
 
-from random import randint
+
+def median(aList,l,r):
+    a = aList[l]
+    b = aList[r]
+    mid = ((l+r)//2)-1 if (l+r) % 2 == 0 else (l+r)//2
+    c = aList[mid]
+    temp = [a,b,c]
+    median = sorted([a,b,c])[1]
+    if median == temp[1]:
+        aList[l], aList[r] = aList[r], aList[l]
+    elif median == temp[2]:
+        aList[l], aList[mid] = aList[mid], aList[l]
+    return median
 
 
 def Partition(aList,l,r):
-    c = randint(l,r-1)
-    aList[l], aList[c] = aList[c], aList[l]
-    pivot = aList[l]
+    pivot = median(aList,l,r-1)
+
     i = l+1
     for j in range(l+1,r):
         if aList[j] <= pivot:
